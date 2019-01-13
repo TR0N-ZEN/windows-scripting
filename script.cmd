53cbc5f5d58ac6fa7f1898415fa6e95aa29f5f81
Theo Reichert <theo.reichert7@gmail.com>
2019-01-13 12:30:48 +0100

    3

--- script.cmd
+++ script.cmd
@@ -1,6 +1,7 @@
 echo("hello world")
-cd G:
-copy G:\sqlite3.dir C:\
-echo("sqlite3 copied into 'C:/sqlite3', now add environmentvariables")
-setx sqlite3 C:\sqlite3\ /m
-setx sqlite3 %PATH%
+C:
+mkdir sqlitetest
+G:
+copy sqlite3 "C:\sqlitetest"
+setx sqlite3 C:\sqlitetest\sqlite\ /m
+pause
