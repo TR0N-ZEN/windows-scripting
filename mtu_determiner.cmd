b6ad0b11a11f39d80183a048ff70ccde1ee90c88
TR0N <theo.reichert7@gmail.com>
2019-01-26 22:34:12 +0100

    n4

---
+++ mtu_determiner.cmd
@@ -0,0 +1,5 @@
+set number = 1500
+set f
+ping www.dslreports.com -f -l 1500 | %f%
+echo %f%
+pause
--- parser.py
+++ parser.py
@@ -1,4 +1,8 @@
-inputfile = open("text.txt", r)
-for lines in inputfile:
-    if "connected" in lines:
-        print(lines)
+inputfile = open("text.txt")
+lines = inputfile.readlines()
+print(lines)
+for x in lines:
+    if " connected" in x:
+        print(x[1:3])
+        outputfile = open("idx.txt", "w")
+        outputfile.write(x[1:3])
