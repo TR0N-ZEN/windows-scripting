53edddc770bd969c001b6bca0326e515aafc6195
Theo Reichert <theo.reichert7@gmail.com>
2019-03-15 10:09:15 +0100

    1.1

--- win10_bloadwareapps_deinstallation.ps1
+++ win10_bloadwareapps_deinstallation.ps1
@@ -11,7 +11,16 @@ Get-AppxPackage -AllUsers *xboxspeech* | Remove-AppxPackage
 Get-AppxPackage -AllUsers *xbox.TCUI* | Remove-AppxPackage
 Get-AppxPackage -AllUsers *yourphone* | Remove-AppxPackage
 Get-AppxPackage -AllUsers *identityprovider* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *solitaire* | Remove-AppxPackage
+Get-AppxPackage *solitairecollection* | Remove-AppxPackage
 Get-AppxPackage -AllUsers *people* | Remove-AppxPackage
 Get-AppxPackage -AllUsers *zune* | Remove-AppxPackage
 Get-AppxPackage -AllUsers *sticky* | Remove-AppxPackage
+Get-AppxPackage *getstarted* | Remove-AppxPackage
+Get-AppxPackage *skypeapp* | Remove-AppxPackage
+Get-AppxPackage *officehub* | Remove-AppxPackage
+Get-AppxPackage *windowsmaps* | Remove-AppxPackage
+Get-AppxPackage *zunevideo* | Remove-AppxPackage
+Get-AppxPackage *bingfinance* | Remove-AppxPackage
+Get-AppxPackage *bingnews* | Remove-AppxPackage
+Get-AppxPackage *windowsphone* | Remove-AppxPackage
+Get-AppxPackage *bingsports* | Remove-AppxPackage
