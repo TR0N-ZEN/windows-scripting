1e63c26d1fffe88819df4ad22e3bbd1b98387bca
Theo <theo.reichert7@gmail.com>
2019-05-06 12:38:10 +0200

    10.0

--- parser.py
+++ parser.py
@@ -1,8 +1,12 @@
+import os
+
 inputfile = open("text.txt")
 lines = inputfile.readlines()
-print(lines)
+cache = []
 for x in lines:
     if " connected" in x:
         print(x[1:3])
-        outputfile = open("idx.txt", "w")
-        outputfile.write(x[1:3])
+        cache.append(x[1:3])
+outputfile = open("idx.txt", "w")
+for i in cache:
+        outputfile.write(i + "\n")
