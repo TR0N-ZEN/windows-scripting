988d0ddb78498cc5a5cd521ce5a0fcde2f90904f
Theo Reichert <theo.reichert7@gmail.com>
2020-03-20 15:13:53 +0100

    20.03.2020

--- bloadware deinstallation/short2.ps1
+++
@@ -1,3 +0,0 @@
-<# Get-AppXProvisionedPackage -online | Remove-AppxProvisionedPackage -online #>
-$exceptions = @('*store*', '*netflix*'. '*whatsapp*', '*spotify*', '*twitter*')
-Get-AppXPackage -allusers | where-object {$_.name -notlike $exceptions[0..-1]} | Remove-AppxPackage
