77f8ebc6716dee6ee704fc4777838a4155b5fcae
Theo Reichert <38770238+TR0N-ZEN@users.noreply.github.com>
2021-06-05 20:23:02 +0200

    Update windows10_setup_via_chocolatey.ps1
    reorderd

--- package manager chocolatey/windows10_setup_via_chocolatey.ps1
+++ package manager chocolatey/windows10_setup_via_chocolatey.ps1
@@ -1,15 +1,18 @@
-choco install vlc
-choco install spotify
+choco install git.install
+git config --global user.name = "Theo Reichert"
+git congfig --global user.email = "theo.reichert7@gmail.com"
 choco install vscode
 choco install mingw
 choco install jdk11
 choco install nodejs
 choco install python3
+choco install firefox //failling
 choco install filezilla
 choco install openvpn
 choco install obs-studio
 choco install xournalplusplus
 choco install megasync
-choco install firefox //failling
 choco install thunderbird //failling
 choco install discord
+choco install vlc
+choco install spotify
