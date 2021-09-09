fe441a036cd99c19c08cbbfe39d6ea74ba6c3d96
TR0N-ZEN <theo.reichert7@gmail.com>
2021-09-09 14:53:53 +0200

    added filter function

--- bloadware_deinstallation/expliciti.ps1
+++
@@ -1,7 +0,0 @@
-Get-AppXPackage -allusers | where-object Publisher -NotMatch 'Microsoft';
-
-$blacklist = 'advert','bing','candy','cooking','cortana','fitbit','getstarted','identityprovider','oneconnect','people','solitaire','skypeapp','sticky','windowsmaps','windowsphone','yourphone','xing','zune';
-
-foreach ($item in $blacklist) {
-	Get-AppXPackage -allusers | where-object Name -match $item;
-}
