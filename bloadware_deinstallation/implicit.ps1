cbb1d6f8057830fc61e7655eea60d10cf677ac93
TR0N-ZEN <theo.reichert7@gmail.com>
2021-09-09 12:21:23 +0200

    learning; next up lists, arrays, hashtables....

--- bloadware_deinstallation/implicit.ps1
+++ bloadware_deinstallation/implicit.ps1
@@ -1,1 +1,1 @@
-Get-AppXPackage -allusers | where-object {$_.Publisher -NotMatch "Microsoft" -and $_.Name -NotMatch "Okular" -and $_.Name -NotMatch "Whatsapp" -and $_.Name -NotMatch "Spotify"} #| Remove-AppxPackage
+Get-AppXPackage -allusers | where-object {$_.Publisher -NotMatch 'Microsoft' -and $_.Name -NotMatch 'Okular' -and $_.Name -NotMatch 'Whatsapp' -and $_.Name -NotMatch 'Spotify' -and $_.Name -notmatch 'Word' -and $_.Name -notmatch 'Excel' -and $_.Name -notmatch 'PowerPoint'} #| Remove-AppxPackage
