6bee49bfa9ba3007b98eae8b59996792a2d46a70
TR0N-ZEN <theo.reichert7@gmail.com>
2021-09-09 14:53:31 +0200

    imprvd bloadware_deinstallation wth filter functn

--- bloadware_deinstallation/expliciti.ps1
+++ bloadware_deinstallation/expliciti.ps1
@@ -1,33 +1,7 @@
-Get-AppXPackage -allusers | where-object { ($_.Name -match 'xbox') -or ($_.Name -match 'zune') -or ($_.Name -match 'YourPhone')} | Remove-AppxPackage
+Get-AppXPackage -allusers | where-object Publisher -NotMatch 'Microsoft';
 
+$blacklist = 'advert','bing','candy','cooking','cortana','fitbit','getstarted','identityprovider','oneconnect','people','solitaire','skypeapp','sticky','windowsmaps','windowsphone','yourphone','xing','zune';
 
-Get-AppXProvisionedPackage -online | Remove-AppxProvisionedPackage -online
-Get-AppxPackage -AllUsers *advert* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *bing* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *cortana* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *office* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *oneconnect* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *xboxapp* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *xboxgameoverlay* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *xboxgamingoverlay* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *xboxspeech* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *xbox.TCUI* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *yourphone* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *identityprovider* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *solitairecollection* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *people* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *zune* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *sticky* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *getstarted* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *skypeapp* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *officehub* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *windowsmaps* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *zune* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *bingfinance* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *bingnews* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *windowsphone* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *bingsports* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *xing* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *fitbit* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *cooking* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *candy* | Remove-AppxPackage
+foreach ($item in $blacklist) {
+	Get-AppXPackage -allusers | where-object Name -match $item;
+}
