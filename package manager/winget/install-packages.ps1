d456d1cb75acc407500c36d01fec5b8d4ee20b73
= <=>
2021-10-26 01:50:48 +0200

    ordered basics.md,  added winget

---
+++ package manager/winget/install-packages.ps1
@@ -0,0 +1,18 @@
+winget install git
+winget install Mega.MEGASync
+winget install Mozilla.Firefox
+winget install BraveSoftware Brave-Browser
+winget install Mozilla.Thunderbird
+winget install Microsoft.VisualStudio.2019.Community
+winget install Win32DiskImager
+winget install OpenJS.NodeJS
+winget install Python.Python.3
+winget install 
+winget install 
+
+winget install VideoLAN.VLC
+
+winget install Discord.Discord
+winget install Valve.Steam
+winget install Ubisoft.Connect
+winget install EpicGames.EpicGamesLauncher
