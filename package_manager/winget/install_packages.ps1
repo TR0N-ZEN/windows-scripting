dd7ee184cc2231175b40bb66ca98e93ded169052
TR0N-ZEN <theo.reichert7@gmail.com>
2021-12-02 00:12:28 +0100

    added winget scripts and registry scripts

---
+++ package_manager/winget/install_packages.ps1
@@ -0,0 +1,28 @@
+winget install GnuPG.Gpg4win
+winget install Git.Git
+# git config --global user.name = "Theo Reichert"
+# git config --global user.email = "theo.reichert7@gmail.com"
+
+winget install Mozilla.Firefox
+winget install BraveSoftware.BraveBrowser
+winget install Mozilla.Thunderbird
+winget install Mega.MEGASync
+
+winget install Microsoft.VisualStudioCode
+winget install Microsoft.VisualStudio.2019.Community
+winget install BellSoft.LibericaJDK.17
+winget install JetBrains.IntelliJIDEA.Community
+winget install OpenJS.NodeJS.LTS
+winget install Python.Python.3
+winget install OpenVPNTechnologies.OpenVPN
+winget install TimKosse.FileZilla.Client
+
+winget install OBSProject.OBSStudio
+winget install OBSProject.obs-amd-encoder
+winget install Discord.Discord
+winget install VideoLAN.VLC
+winget install Spotify.Spotify
+
+winget install xournalplusplus
+
+# winget search "Norton Security Protection"
