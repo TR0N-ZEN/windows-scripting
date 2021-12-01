1d577273b784610c39337194ba253bbe6bd5d87f
TR0N-ZEN <theo.reichert7@gmail.com>
2021-12-02 00:12:28 +0100

    added winget scripts and registry scripts

--- windows_settings/printer.ps1
+++ windows_settings/printer.ps1
@@ -2,4 +2,4 @@
 # Rename-Printer 'Canon TR4500 series' 'DD USB'
 # Remove-Printer 'Canon TR4500 series FAX'
 
-Set-PrintConfiguration -PrinterName 'Canon TR4500 series'-Collate $true -Color $false -DuplexingMode TwoSidedLongEdge -PaperSize A4
+Set-PrintConfiguration -PrinterName 'Canon TR4500 series' -Collate $true -Color $false -DuplexingMode TwoSidedLongEdge -PaperSize A4
