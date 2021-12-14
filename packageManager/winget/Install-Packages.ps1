58a70358ea180a91bc83bba79c8a4b44b57254b6
= <=>
2021-12-14 16:25:26 +0100

    minor changes

--- packageManager/winget/Install-Packages.ps1
+++ packageManager/winget/Install-Packages.ps1
@@ -2,11 +2,12 @@ winget install GnuPG.Gpg4win
 winget install Git.Git
 
 winget install Mozilla.Firefox
-winget install BraveSoftware.BraveBrowser
+#winget install BraveSoftware.BraveBrowser
 winget install Mozilla.Thunderbird
 winget install Mega.MEGASync
 
-winget install Microsoft.VC++2015-2022Redist-x64
+#winget install Microsoft.VC++2015-2022Redist-x86
+#winget install Microsoft.VC++2015-2022Redist-x64
 
 winget install Microsoft.VisualStudioCode
 winget install Microsoft.VisualStudio.2019.Community
@@ -15,16 +16,16 @@ winget install JetBrains.IntelliJIDEA.Community
 winget install OpenJS.NodeJS.LTS
 winget install Python.Python.3
 winget install OpenVPNTechnologies.OpenVPN
-winget install TimKosse.FileZilla.Client
+# winget install TimKosse.FileZilla.Client
 # winget install Win32DiskImager
 
 winget install OBSProject.OBSStudio
-winget install OBSProject.obs-amd-encoder
+# winget install OBSProject.obs-amd-encoder
 winget install Discord.Discord
 winget install VideoLAN.VLC
 winget install Spotify.Spotify
 
-winget install xournalplusplus
+# winget install xournalplusplus
 
 winget install Valve.Steam
 winget install Ubisoft.Connect
