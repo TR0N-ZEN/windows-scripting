88ce2f876758472e8ce69fb18f4b4c7399f4ab6f
Theo Reichert <theo.reichert7@gmail.com>
2022-02-22 03:52:48 +0100

    sorting packages

--- packageManager/winget/Install-Packages.ps1
+++ packageManager/winget/Install-Packages.ps1
@@ -1,22 +1,24 @@
-# winget search "Norton Security Protection"
+# winget install "Norton Security Protection"
 winget install GnuPG.Gpg4win
 winget install Git.Git
 # winget install Microsoft.VC++2015-2022Redist-x64
 winget install 7zip.7zip
 
+
 winget install Mozilla.Firefox
-#winget install BraveSoftware.BraveBrowser
+# winget install BraveSoftware.BraveBrowser
 winget install Mozilla.Thunderbird
 winget install Mega.MEGASync
 
-winget install Spotify.Spotify
-winget install OBSProject.OBSStudio
+winget install VideoLAN.VLC
+# winget install Spotify.Spotify
+# winget install OBSProject.OBSStudio
 # winget install OBSProject.obs-amd-encoder
 winget install Discord.Discord
-winget install VideoLAN.VLC
+
 
 winget install Microsoft.VisualStudioCode
-winget install Microsoft.VisualStudio.2019.Community
+# winget install Microsoft.VisualStudio.2019.Community
 # winget install BellSoft.LibericaJDK.17
 winget install JetBrains.IntelliJIDEA.Community
 winget install OpenJS.NodeJS.LTS
