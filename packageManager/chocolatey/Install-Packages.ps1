4dbd21e01ae3afaed1205ded4965f7a3fdd993f5
Theo Reichert <theo.reichert7@gmail.com>
2022-03-23 21:27:35 +0100

    haskell development environment

--- packageManager/chocolatey/Install-Packages.ps1
+++ packageManager/chocolatey/Install-Packages.ps1
@@ -1,18 +1,20 @@
 # Set-ExecutionPolicy Unrestricted
-choco install git
-choco install vscode
-choco install mingw
-choco install jdk11
-choco install nodejs
-choco install python3
-choco install firefox
-choco install filezilla
-choco install openvpn
-choco install obs-studio
-choco install xournalplusplus
-choco install megasync
-choco install thunderbird
-choco install discord
-choco install vlc
-choco install spotify
-Set-ExecutionPolicy Restricted
+# choco install git
+# choco install vscode
+# choco install mingw
+# choco install jdk11
+# choco install nodejs
+# choco install python3
+# choco install firefox
+# choco install filezilla
+# choco install openvpn
+# choco install obs-studio
+# choco install xournalplusplus
+# choco install megasync
+# choco install thunderbird
+# choco install discord
+# choco install vlc
+# choco install spotify
+choco install haskell-dev haskell-stack
+refreshenv
+# Set-ExecutionPolicy Restricted
