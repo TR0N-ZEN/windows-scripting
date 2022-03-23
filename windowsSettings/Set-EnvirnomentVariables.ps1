bc06780bdf4eab14c84fc6528dc4a67cce655845
Theo Reichert <theo.reichert7@gmail.com>
2022-03-23 21:26:58 +0100

    minorities

--- windowsSettings/Set-EnvirnomentVariables.ps1
+++ windowsSettings/Set-EnvirnomentVariables.ps1
@@ -1,8 +1,9 @@
-Set-Item -Path Env:TMP -Value Z:\ego\tmp\
-Set-Item -Path Env:TEMP -Value Z:\ego\temp\
-Set-Item -Path Env:APPDATA -Value Z:\ego\AppData\
+# Set-Item -Path Env:TMP -Value Z:\ego\tmp\
+# Set-Item -Path Env:TEMP -Value Z:\ego\temp\
+# Set-Item -Path Env:APPDATA -Value Z:\ego\AppData\
 
 # Get-Item -Path Env:
 
-$HOME2 = "D:\home\ego\"
-$STUDIES = "D:\home\ego\Documents\MEGA\studies\"
+$HOME2 = 'D:\home\ego\'
+$STUDIES = 'D:\home\ego\Documents\MEGA\studies\'
+
