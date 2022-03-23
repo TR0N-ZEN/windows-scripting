cdf0fb9a342f8db59e802e3a11835e55c6e47c63
Theo Reichert <theo.reichert7@gmail.com>
2022-03-23 21:22:53 +0100

    minorities

--- packageManager/winget/Install-Packages.ps1
+++ packageManager/winget/Install-Packages.ps1
@@ -1,5 +1,4 @@
 # winget install "Norton Security Protection"
-# winget search 9NKK9HHB8HHF
 # winget install 9NKK9HHB8HHF # "Norton Security Protection" from msstore
 winget install "GnuPG.Gpg4win"
 winget install "Git.Git"
@@ -15,8 +14,8 @@ winget install "Mega.MEGASync"
 #winget install Spotify.Spotify
 winget install 9NCBCSZSJRSB # spotify from msstore
 
-#winget install OBSProject.OBSStudio
-#winget install OBSProject.obs-amd-encoder
+#winget install "OBSProject.OBSStudio"
+#winget install "OBSProject.obs-amd-encoder"
 # winget install "Discord.Discord"
 winget install XPDC2RH70K22MN # discord from msstore
 
@@ -26,16 +25,16 @@ winget install "Microsoft.VisualStudioCode"
 winget install "OpenJS.NodeJS.LTS"
 winget install "Python.Python.3"
 winget install "OpenVPNTechnologies.OpenVPN"
-#winget install Microsoft.VisualStudio.2019.Community
-#winget install BellSoft.LibericaJDK.17
-#winget install JetBrains.IntelliJIDEA.Community
-# winget install TimKosse.FileZilla.Client
-# winget install Win32DiskImager
+# winget install "Microsoft.VisualStudio.2019.Community"
+# winget install "BellSoft.LibericaJDK.17"
+# winget install "JetBrains.IntelliJIDEA.Community"
+# winget install "TimKosse.FileZilla.Client"
+# winget install "Win32DiskImager"
 winget install "xournalplusplus"
 
-#winget install Valve.Steam
-#winget install Ubisoft.Connect
-#winget install EpicGames.EpicGamesLauncher
-#winget install ElectronicArts.EADesktop
+#winget install "Valve.Steam"
+#winget install "Ubisoft.Connect"
+#winget install "EpicGames.EpicGamesLauncher"
+#winget install "ElectronicArts.EADesktop"
 
 winget install 9PMK584KQVC2 # Canon Inkjet Smart Connect
