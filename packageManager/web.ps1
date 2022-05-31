12308d297635a1f309d7d8042f28a51a0af4325d
Theo Reichert <theo.reichert7@gmail.com>
2022-05-31 14:56:25 +0200

    improving installtion form files in web

--- packageManager/web.ps1
+++ packageManager/web.ps1
@@ -1,12 +1,23 @@
-Invoke-WebRequest -Uri "https://download.msi.com/uti_exe/vga/MSIAfterburnerSetup.zip?__token__=exp=1639666903~acl=/*~hmac=918f33057316005f2f9cf1b98bbd98945034054e83ffe389e77793a5c45e80d4" -O "MSIAfterburner.zip" -UseBasicParsing  # download zipped MSI Afterburner installer
-Expand-Archive "MSIAfterburner.zip"
-. ./MSIAfterburner/MSIAfterburnerSetup*.exe
+# gpu hardware sureveillance and control over fans, voltage and clocks
+Invoke-WebRequest -Uri 'https://download.msi.com/uti_exe/vga/MSIAfterburnerSetup.zip?__token__=exp=1639666903~acl=/*~hmac=918f33057316005f2f9cf1b98bbd98945034054e83ffe389e77793a5c45e80d4' -O 'MSIAfterburner.zip' -UseBasicParsing  # download zipped MSI Afterburner installer
+Expand-Archive 'MSIAfterburner.zip'
+. "./MSIAfterburner/MSIAfterburnerSetup*.exe"
 
-#https://download.heise.de/files/lvZujkGWyangWJZ00X9m5A/266081/imdiskinst.exe?expires=1640230424
 
+# ramdisk software
+Invoke-WebRequest -Uri 'https://download.heise.de/files/lvZujkGWyangWJZ00X9m5A/266081/imdiskinst.exe?expires=1640230424' -O 'imdisk.installer.exe'
+. '.\imdisk.installer.exe'
+
+# glasgow haskell compiler for haskell
 # Invoke-WebRequest -Uri 'https://downloads.haskell.org/~ghc/9.0.2/ghc-9.0.2-x86_64-unknown-mingw32.tar.xz' -O 'ghc-9.0.2-x86_64-unknown-mingw32.tar.xz' -UseBasicParsing
+# . '.\ghc-9.0.2-x86_64-unknown-mingw32.tar.xz'
 
-
+# audio equalizer 
 Invoke-WebRequest -Uri 'https://equalizerapo.com/EqualizerAPO64-1.2.1.zip' -O 'EqualizerAPO64-1.2.1.zip' -UseBasicParsing
 Expand-Archive 'EqualizerAPO64-1.2.1.zip'
-. .\EqualizerAPO64-1.2.1\EqualizerAPO64-1.2.1.exe
+. '.\EqualizerAPO64-1.2.1\EqualizerAPO64-1.2.1.exe'
+
+# fan control
+Invoke-WebRequest -Uri 'https://github.com/Rem0o/FanControl.Releases/blob/master/FanControl.zip?raw=true' -O 'FanControl.zip' -UseBasicParsing
+Expand-Archive 'FanControl.zip'
+. '.\FanControl\FanControl.exe'
