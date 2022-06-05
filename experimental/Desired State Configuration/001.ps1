e5a3958890c75d831de3d52b38116a1a365b3521
Theo Reichert <theo.reichert7@gmail.com>
2022-06-06 00:36:33 +0200

    initialiye DesiredStateConfiguration

---
+++ experimental/Desired State Configuration/001.ps1
@@ -0,0 +1,5 @@
+Install-Module -Name PSDesiredStateConfiguration -Repository PSGallery -MaximumVersion 2.99
+Import-Module -Name PSDesiredStateConfiguration
+
+# Install-Module -Name ComputerManagementDsc -Repository PSGallery
+# Import-Module -Name ComputerManagementDsc
