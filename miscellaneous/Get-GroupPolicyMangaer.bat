66f444fd3d30990f51179a0a7ea88af7a7769bb3
Theo Reichert <theo.reichert7@gmail.com>
2022-08-16 00:28:27 +0200

    install group policy manager

---
+++ miscellaneous/Get-GroupPolicyMangaer.bat
@@ -0,0 +1,2 @@
+FOR %F IN ("%SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package~*.mum") DO (DISM /Online /NoRestart /Add-Package:"%F")
+FOR %F IN ("%SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package~*.mum") DO (DISM /Online /NoRestart /Add-Package:"%F")
