dcf72b498922cd20c58c42712e0cd38130a74e52
Theo Reichert <theo.reichert7@gmail.com>
2022-10-09 22:07:54 +0200

    added services to disable, maybe broke order of dependencies

--- windowsSettings/Set-Services.ps1
+++ windowsSettings/Set-Services.ps1
@@ -1,50 +1,70 @@
 # Set-WindowsSearchSetting -EnableWebResultsSetting $false -EnableMeteredWebResultsSetting $false -SearchExperienceSetting NotPersonalized -SafeSearchSetting Off
 
 $service_to_be_disabled = @(
-  'WSearch',
-  'AxInstSV',
-  'FrameServer',
-  'tzautoupdate',
-  'bthserv',
-  'dmwappushservice',
-  'MapsBroker',
-  'lfsvc',
-  'SharedAccess',
-  'lltdsvc',
   'AppVClient',
-  'NetTcpPortSharing',
+  'AxInstSV',
   'CscService',
+  'DoSvc',
+  'DusmSvc',
+  'FrameServer',
+  'IBMPMSVC',
+  'MapsBroker',
+  'NetTcpPortSharing',
+  'OneSyncSvc_8aed0',
   'PhoneSvc',
-  # 'Spooler',
-  # 'PrintNotify',
   'QWAVE',
-  'RmSvc',
   'RemoteAccess',
-  'SensorDataService',
-  'SensrSvc',
-  'SensorService',
-  # 'ShellHWDetection',
+  'RmSvc',
   'SCardSvr',
-  'ScDeviceEnum',
-  'SSDPSRV',
-  'WiaRpc',
-  'TabletInputService',
-  'upnphost',
-  'UserDataSvc',
-  'UevAgentService',
-  'WalletService',
   'SEMgrSvc',
-  'stisvc',
-  'wisvc',
-  'icssvc',
+  'SSDPSRV',
+  'ScDeviceEnum',
+  'SensorDataService',
+  'SensorService',
+  'SensrSvc',
+  'SharedAccess',
+  'TabletInputService',
+  'UevAgentService',
+  'UserDataSvc',
+  'WSearch',
+  'WalletService',
+  'WbioSrvc',
+  'WiaRpc',
   'XblAuthManager',
   'XblGameSave',
   'XboxGipSvc',
   'XboxNetApiSvc'
+  'bthserv',
+  'dmwappushservice',
+  'icssvc',
+  'lfsvc',
+  'lltdsvc',
+  'RasAuto',
+  'RasMan',
+  'RemoteAccess',
+  'stisvc',
+  'TokenBroker',
+  'tzautoupdate',
+  'upnphost',
+  'VaultSvc',
+  'WbioSrvc',
+  'wisvc'
 )
+  # 'Spooler',
+  # 'PrintNotify',
+  # 'ShellHWDetection',
 
 foreach ($service in $service_to_be_disabled) {
     # Set-Service -Name $service -Status Stopped
     Get-Service -Name $service | Stop-Service
     Set-Service $service -StartupType Disabled
 }
+
+Get-Service | where-Object Name -match Razer | Set-Service -StartupType Disabled
+Get-Service | where-Object Name -match Razer | Stop-Service
+
+Get-Service | where-Object Name -match Logi | Set-Service -StartupType Disabled
+Get-Process | Where-Object Name -match Logi | Stop-Process
+
+Get-Service | where-Object Name -match Zoom | Set-Service -StartupType Disabled
+Get-Process | Where-Object Name -match Zoom | Stop-Process
