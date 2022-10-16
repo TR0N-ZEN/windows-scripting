34ee04230d2ed833efeaa18e8397410051488bc4
Theo Reichert <theo.reichert7@gmail.com>
2022-10-16 22:21:20 +0200

    using foreach loop

--- packageManager/winget/Install-Packages.ps1
+++ packageManager/winget/Install-Packages.ps1
@@ -1,34 +1,45 @@
-# winget install "Norton Security Protection"
-winget install 9NKK9HHB8HHF # "Norton Security Protection" from msstore
-winget install "GnuPG.Gpg4win"
-winget install "Git.Git"
-winget install "Microsoft.VC++2015-2022Redist-x64"
-winget install "7zip.7zip"
-#winget install Mozilla.Firefox
-winget install 9NZVDKPMR9RD #firefox from msstore
-#winget install BraveSoftware.BraveBrowser
-winget install "Mozilla.Thunderbird"
-winget install "Mega.MEGASync"
-#winget install Spotify.Spotify
-winget install 9NCBCSZSJRSB # spotify from msstore
-winget install "GoLang.Go"
-#winget install "OBSProject.OBSStudio"
-#winget install "OBSProject.obs-amd-encoder"
-# winget install "Discord.Discord"
-winget install XPDC2RH70K22MN # discord from msstore
-winget install "VideoLAN.VLC"
-winget install "Microsoft.VisualStudioCode"
-winget install "OpenJS.NodeJS.LTS"
-winget install "Python.Python.3"
-winget install "OpenVPNTechnologies.OpenVPN"
-# winget install "Microsoft.VisualStudio.2019.Community"
-# winget install "BellSoft.LibericaJDK.17"
-# winget install "JetBrains.IntelliJIDEA.Community"
-# winget install "TimKosse.FileZilla.Client"
-# winget install "Win32DiskImager"
-winget install "xournalplusplus"
-winget install "Valve.Steam"
-winget install "Ubisoft.Connect"
-winget install "EpicGames.EpicGamesLauncher"
-winget install "ElectronicArts.EADesktop"
-winget install 9PMK584KQVC2 # Canon Inkjet Smart Connect
+$programs = 
+# 'Norton Security Protection'
+# 'Norton Security Protection' from msstore
+9NKK9HHB8HHF, 
+'GnuPG.Gpg4win',
+'Git.Git',
+'Microsoft.VC++2015-2022Redist-x64',
+'7zip.7zip',
+#Mozilla.Firefox,
+#firefox from msstore,
+9NZVDKPMR9RD, 
+#BraveSoftware.BraveBrowser,
+'Mozilla.Thunderbird',
+'Mega.MEGASync',
+#Spotify.Spotify,
+# spotify from msstore
+9NCBCSZSJRSB,
+'GoLang.Go',
+#'OBSProject.OBSStudio',
+#'OBSProject.obs-amd-encoder',
+# 'Discord.Discord',
+# discord from msstore
+XPDC2RH70K22MN, 
+# 'VideoLAN.VLC',
+'Microsoft.VisualStudioCode',
+# 'OpenJS.NodeJS.LTS',
+'Python.Python.3',
+'OpenVPNTechnologies.OpenVPN',
+# 'Microsoft.VisualStudio.2019.Community',
+# 'BellSoft.LibericaJDK.17',
+# 'JetBrains.IntelliJIDEA.Community',
+# 'TimKosse.FileZilla.Client',
+# 'Win32DiskImager',
+'xournalplusplus',
+'Valve.Steam',
+'Ubisoft.Connect',
+'EpicGames.EpicGamesLauncher',
+'ElectronicArts.EADesktop',
+# Canon Inkjet Smart Connect
+'9PMK584KQVC2'
+
+
+foreach ($program in $programs) {
+  winget install $program
+}
