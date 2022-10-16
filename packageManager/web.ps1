98cf3eac827b360ec23df8c95df27c2485c8ceba
Theo Reichert <theo.reichert7@gmail.com>
2022-10-16 22:21:56 +0200

    set-location  to downloadfolder; excluded som pkgs

--- packageManager/web.ps1
+++ packageManager/web.ps1
@@ -1,11 +1,14 @@
-# msi afterburner
-Invoke-WebRequest -Uri 'https://download.msi.com/uti_exe/vga/MSIAfterburnerSetup.zip?__token__=exp=1639666903~acl=/*~hmac=918f33057316005f2f9cf1b98bbd98945034054e83ffe389e77793a5c45e80d4' -O 'MSIAfterburner.zip' -UseBasicParsing  # download zipped MSI Afterburner installer
-Expand-Archive 'MSIAfterburner.zip'
-. "./MSIAfterburner/MSIAfterburnerSetup*.exe"
+Set-Location (Get-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders").PSObject.Properties["{374DE290-123F-4565-9164-39C4925E467B}"].Value
 
-# ramdisk software
-Invoke-WebRequest -Uri 'https://download.heise.de/files/lvZujkGWyangWJZ00X9m5A/266081/imdiskinst.exe?expires=1640230424' -O 'imdisk.installer.exe'
-. '.\imdisk.installer.exe'
+
+# # msi afterburner
+# Invoke-WebRequest -Uri 'https://download.msi.com/uti_exe/vga/MSIAfterburnerSetup.zip?__token__=exp=1639666903~acl=/*~hmac=918f33057316005f2f9cf1b98bbd98945034054e83ffe389e77793a5c45e80d4' -O 'MSIAfterburner.zip' -UseBasicParsing  # download zipped MSI Afterburner installer
+# Expand-Archive 'MSIAfterburner.zip'
+# . "./MSIAfterburner/MSIAfterburnerSetup*.exe"
+
+# # ramdisk software
+# Invoke-WebRequest -Uri 'https://download.heise.de/files/lvZujkGWyangWJZ00X9m5A/266081/imdiskinst.exe?expires=1640230424' -O 'imdisk.installer.exe'
+# . '.\imdisk.installer.exe'
 
 # glasgow haskell compiler for haskell
 # Invoke-WebRequest -Uri 'https://downloads.haskell.org/~ghc/9.0.2/ghc-9.0.2-x86_64-unknown-mingw32.tar.xz' -O 'ghc-9.0.2-x86_64-unknown-mingw32.tar.xz' -UseBasicParsing
@@ -16,10 +19,10 @@ Invoke-WebRequest -Uri 'https://equalizerapo.com/EqualizerAPO64-1.2.1.zip' -O 'E
 Expand-Archive 'EqualizerAPO64-1.2.1.zip'
 . '.\EqualizerAPO64-1.2.1\EqualizerAPO64-1.2.1.exe'
 
-# fan control  
-Invoke-WebRequest -Uri 'https://github.com/Rem0o/FanControl.Releases/blob/master/FanControl.zip?raw=true' -O 'FanControl.zip' -UseBasicParsing
-Expand-Archive 'FanControl.zip'
-. '.\FanControl\FanControl.exe'
+# # fan control  
+# Invoke-WebRequest -Uri 'https://github.com/Rem0o/FanControl.Releases/blob/master/FanControl.zip?raw=true' -O 'FanControl.zip' -UseBasicParsing
+# Expand-Archive 'FanControl.zip'
+# . '.\FanControl\FanControl.exe'
 
 # battle net
 Invoke-WebRequest -Uri 'https://www.battle.net/download/getInstallerForGame?os=win&gameProgram=BATTLENET_APP&version=Live&id=undefined' -OutFile  BattleNet.installer.exe
