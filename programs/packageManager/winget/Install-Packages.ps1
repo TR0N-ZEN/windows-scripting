d1daa56514ad026e7061db6859f10931e08bdcb7
Theo Reichert <theo.reichert7@gmail.com>
2023-06-20 19:51:17 +0200

    updated used programs and categorized in basics,dev and gaming

--- programs/packageManager/winget/Install-Packages.ps1
+++ programs/packageManager/winget/Install-Packages.ps1
@@ -1,45 +1,45 @@
-$programs = 
-# 'Norton Security Protection'
+$basics = 
 # 'Norton Security Protection' from msstore
-9NKK9HHB8HHF, 
-'GnuPG.Gpg4win',
-'Git.Git',
+'9NKK9HHB8HHF', 
+# BraveSoftware.BraveBrowser for mstore
+'XP8C9QZMS2PC1T',
+# firefox from msstore
+#9NZVDKPMR9RD, 
 'Microsoft.VC++2015-2022Redist-x64',
+'GnuPG.Gpg4win',
 '7zip.7zip',
-#Mozilla.Firefox,
-#firefox from msstore,
-9NZVDKPMR9RD, 
-#BraveSoftware.BraveBrowser,
-'Mozilla.Thunderbird',
-'Mega.MEGASync',
-#Spotify.Spotify,
+'xournalplusplus',
+# 'VideoLAN.VLC',
+# Canon Inkjet Smart Connect
+'9PMK584KQVC2',
+'OpenVPNTechnologies.OpenVPN',
 # spotify from msstore
-9NCBCSZSJRSB,
-'GoLang.Go',
+'9NCBCSZSJRSB'
+
+$dev =
+'Git.Git',
+#'GoLang.Go',
 #'OBSProject.OBSStudio',
 #'OBSProject.obs-amd-encoder',
-# 'Discord.Discord',
-# discord from msstore
-XPDC2RH70K22MN, 
-# 'VideoLAN.VLC',
-'Microsoft.VisualStudioCode',
+'Microsoft.VisualStudioCode'
 # 'OpenJS.NodeJS.LTS',
-'Python.Python.3',
-'OpenVPNTechnologies.OpenVPN',
+# 'Python.Python.3',
 # 'Microsoft.VisualStudio.2019.Community',
 # 'BellSoft.LibericaJDK.17',
 # 'JetBrains.IntelliJIDEA.Community',
 # 'TimKosse.FileZilla.Client',
 # 'Win32DiskImager',
-'xournalplusplus',
+
+
+$gaming =
 'Valve.Steam',
 'Ubisoft.Connect',
 'EpicGames.EpicGamesLauncher',
 'ElectronicArts.EADesktop',
-# Canon Inkjet Smart Connect
-'9PMK584KQVC2'
+# discord from msstore
+'XPDC2RH70K22MN'
 
-
-foreach ($program in $programs) {
+foreach ($program in $basics + $gaming) {
   winget install $program
+  #Write-Host $program
 }
--- programs/packageManager/winget/README.md
+++ programs/packageManager/winget/README.md
@@ -1,31 +1,3 @@
 ```cmd
-PS C:\Windows\system32> winget
-Windows Package Manager v1.1.12653
-Copyright (c) Microsoft Corporation. Alle Rechte vorbehalten.
-
-Das “winget”-Befehlszeilenprogramm ermöglicht das Installieren von Anwendungen und anderen Paketen mithilfe der Befehlszeile.
-
-Nutzung: winget [<Befehl>] [<Optionen>]
-
-Folgende Befehle sind verfügbar:
-  install    Installiert das angegebene Paket
-  show       Zeigt Informationen zu einem Paket an
-  source     Verwalten von Paketquellen
-  search     Suchen und Anzeigen grundlegender Informationen zu Paketen
-  list       Installierte Pakete anzeigen
-  upgrade    Aktualisiert das gegebene Paket
-  uninstall  Deinstalliert das angegebene Paket
-  hash       Hilfsprogramm zum Hashen von Installationsdateien
-  validate   Überprüft eine Manifestdatei
-  settings   Einstellungen öffnen oder Administratoreinstellungen festlegen
-  features   Zeigt den Status von experimentellen Features an
-  export     Exportiert eine Liste der installierten Pakete
-  import     Installiert alle Pakete in einer Datei
-
-Wenn Sie weitere Details zu einem bestimmten Befehl erfahren möchten, übergeben Sie ihm das Hilfe-Argument. [-?]
-
-Die folgenden Optionen stehen zur Verfügung:
-  -v,--version  Version des Tools anzeigen
-  --info        Allgemeine Informationen zum Tool anzeigen
 
 ```
