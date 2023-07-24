d410cea3fc3b0243cd25c72d9c0f8e878e4adb46
Theo Reichert <theo.reichert7@gmail.com>
2023-07-24 02:10:29 +0200

    removed stupid stuff

--- experimental/FilePermissions.md
+++
@@ -1,3 +0,0 @@
-https://stackoverflow.com/questions/22988384/powershell-change-owner-of-files-and-folders
-
-
--- experimental/copy/script.cmd
+++
@@ -1,7 +0,0 @@
-C:
-mkdir "C:\sqlite3"
-G:
-copy sqlite3 "C:\sqlite3\"
-setx sqlite3 C:\sqlite3\ /m
-setx sqlite3 C:\sqlite3\
-pause
