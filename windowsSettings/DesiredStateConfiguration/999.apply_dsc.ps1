2589a776db413b690ca126d174a0b6a5a6f45ac2
Theo Reichert <theo.reichert7@gmail.com>
2023-07-24 02:12:33 +0200

    added DesiredStateConfiguration

---
+++ windowsSettings/DesiredStateConfiguration/999.apply_dsc.ps1
@@ -0,0 +1,15 @@
+# execute in powershell of version 7.*.*
+# from here following: https://learn.microsoft.com/de-de/powershell/dsc/how-tos/configurations/write-and-compile?view=dsc-2.0
+
+
+# https://learn.microsoft.com/en-us/powershell/module/psdesiredstateconfiguration/?source=recommendations&view=dsc-2.0
+# https://learn.microsoft.com/en-us/powershell/dsc/managing-nodes/apply-get-test?source=recommendations&view=dsc-1.1&viewFallbackFrom=dsc-2.0
+
+# this module provides commands with the noun DscConfiguration
+Import-Module -Name PSDesiredStateConfiguration -RequiredVersion 2.0.7
+
+Start-DscConfiguration -Path .\GamingMinimalConfig\ -wait -verbose # -force
+Start-DscConfiguration -Path .\MyBasicConfig\ -wait -verbose # -force
+
+Stop-Service -Name WinRM
+Set-Service -Name WinRM -StartupType Disabled
---
+++ windowsSettings/DesiredStateConfiguration/README.md
@@ -0,0 +1,9 @@
+https://github.com/djdallmann/GamingPCSetup/blob/master/CONTENT/DOCS/PSDSC/README.md
+
+execute scirpts in powershell of version 7.*.*
+
+Execute 001  
+then one of 002-998  
+and after that 999.  
+
+
